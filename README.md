e35755294f10839f58f7dd151fb062926c1a4e44
xiawei <shrimp32@126.com>
2018-03-01 14:52:04 +0800

    将hello作为config的client

--- README.md
+++ README.md
@@ -5,3 +5,12 @@
   - port：2001
 - cloud-config：配置中心
   - port：3000
+  - 配置文件的映射规则
+    - 文件名：{application}-{profile}.properties
+    - /{application}/{profile}[/{label}]
+     /{application}-{profile}.yml
+     /{label}/{application}-{profile}.yml
+     /{application}-{profile}.properties
+     /{label}/{application}-{profile}.properties
+    - {label}对应git上的分支名
+    
--- hello/src/main/java/com/xw/cloud/hello/HelloApplication.java
+++ hello/src/main/java/com/xw/cloud/hello/HelloApplication.java
@@ -1,8 +1,10 @@
 package com.xw.cloud.hello;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -11,4 +13,11 @@ public class HelloApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HelloApplication.class, args);
 	}
+
+	@Value("${app.name}")
+    String name;
+    @RequestMapping(value = "/hi")
+    public String hi(){
+        return "hi,"+name;
+    }
 }
